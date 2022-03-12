90ccda2bd6df63d7707ad9b886ad05ee41611087
rutujafirke <rutujafirke123@gmail.com>
2022-03-12 14:26:57 +0530

    Commiting The Java Code

---
+++ Fibonnacci.java
@@ -0,0 +1,25 @@
+/*11.Write a Java Program  Print the fibonnacci series as follows: -  
+    0	1	1	2	3	5	8	13
+*/
+
+import java.util.Scanner;
+ public class Fibonnacci{
+	public static void main(String args[]){
+		int num1=0,num2=1,num3=0;
+		Scanner sc =new Scanner(System.in);
+
+		System.out.print("Enter the number:");
+		int n=sc.nextInt();
+
+		System.out.print(num1 + " \t" + num2);
+		
+		for(int i=0;i<n;i++)
+		{
+			num3=num1+num2;
+			System.out.print("\t"+num3);
+			num1=num2;
+			num2 = num3;
+					
+		}
+	}
+}
